6131a189b65cfc15eafe28bb3b03b65a1222bfc3
tigerwu <tigerwu74@hotmail.com>
2015-05-18 13:28:00 +0800

    518update

--- MyOne/src/com/example/myone/model/Viewspot.java
+++ MyOne/src/com/example/myone/model/Viewspot.java
@@ -1,8 +1,48 @@
 package com.example.myone.model;
 
-import java.lang.Double;
+import java.util.ArrayList;
+
 
 public class Viewspot {
+	private Long ID;
+	private String Name;
+	private String Descript;
+	private Long ParkID;
+	private ArrayList<Location> Locations;
+	
+	
+	public Long getID() {
+		return ID;
+	}
+	public void setID(Long iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getDescript() {
+		return Descript;
+	}
+	public void setDescript(String descript) {
+		Descript = descript;
+	}
+	
+	public Long getParkID() {
+		return ParkID;
+	}
+	public void setParkID(Long ParkID) {
+		ParkID = ParkID;
+	}
+	public ArrayList<Location> getLocations() {
+		return Locations;
+	}
+	public void setLocations(ArrayList<Location> locations) {
+		Locations = locations;
+	}
 
+	
 	
 }
--- MyOne/src/com/example/myone/service/ILocate.java
+++
@@ -1,7 +0,0 @@
-package com.example.myone.service;
-
-import com.example.myone.model.Location;
-
-public interface ILocate {
-	public Location getLocationNow();
-}
