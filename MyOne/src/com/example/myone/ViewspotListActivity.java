037defeb9bdf2df898c20786611b73ec88c7cb0c
tigerwu <tigerwu74@hotmail.com>
2015-05-28 10:46:26 +0800

    5-28 加入了GPS定位识别播放语音

--- MyOne/src/com/example/myone/ViewspotListActivity.java
+++ MyOne/src/com/example/myone/ViewspotListActivity.java
@@ -26,16 +26,33 @@ import android.widget.Toast;
 
 public class ViewspotListActivity extends Activity {
 	private static final String ACTIVITY_TAG = "ViewspotListActivity";
-	
+
 	private String[] vsarray = null;
 	private MediaPlayer[] mps = null;
 	private VLocation[] locas = null;
-	
+
+	LocationManager locationManager = null;
+
+	private myLocationListener mylocalistener = new myLocationListener();
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		Log.i(ViewspotListActivity.ACTIVITY_TAG, "onStop()");
+		if (locationManager != null) {
+			locationManager.removeUpdates(mylocalistener);
+		}
+		super.onStop();
+	}
+
 	@Override
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
 		int i;
-		for(i = 0; i < 8 ; i++) {
+
+		Log.i(ViewspotListActivity.ACTIVITY_TAG, "onDestroy()");
+
+		for (i = 0; i < 8; i++) {
 			mps[i].release();
 		}
 		super.onDestroy();
@@ -45,15 +62,17 @@ public class ViewspotListActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_viewspot_list);
-		
-		getLocation();
-		
+		// 创建资源
 		createLocalMp3();
-		
-		vsarray = this.getResources().getStringArray(R.array.strarr_spinner_viewspots);
+		createViewLocas();
+		// 定位
+		getLocation();
+		// 初始化列表
+		vsarray = this.getResources().getStringArray(
+				R.array.strarr_spinner_viewspots);
 		ListView lv_viewspot = (ListView) this.findViewById(R.id.lv_viewspots);
 		lv_viewspot.setAdapter(new ViewspotAdapter(vsarray));
-		
+
 		lv_viewspot.setOnItemClickListener(new ViewspotLVListener());
 	}
 
@@ -63,48 +82,37 @@ public class ViewspotListActivity extends Activity {
 		getMenuInflater().inflate(R.menu.viewspot_list, menu);
 		return true;
 	}
-	
+
 	private class ViewspotLVListener implements OnItemClickListener {
 
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position,
 				long id) {
 			// TODO Auto-generated method stub
-			TextView itemtxview = (TextView)view;
+			TextView itemtxview = (TextView) view;
 			String str_viewspot = itemtxview.getText().toString();
-			
-			try {
-				mps[position].prepare();
-			} catch (IllegalStateException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-			mps[position].start();
-			
+
+			PlayMP3(position);
 			DisplayToast(str_viewspot);
-			
+
 		}
-		
+
 	}
-	
+
 	private class ViewspotAdapter extends BaseAdapter {
 		String[] vsarray = null;
-		
+
 		public ViewspotAdapter(String[] vsArray) {
 			// TODO Auto-generated constructor stub
 			vsarray = vsArray;
 		}
-		
+
 		@Override
 		public int getCount() {
 			// TODO Auto-generated method stub
 			if (vsarray == null) {
 				return 0;
-			}
-			else {
+			} else {
 				return vsarray.length;
 			}
 		}
@@ -127,21 +135,48 @@ public class ViewspotListActivity extends Activity {
 			TextView mTextView = null;
 			if (vsarray != null) {
 				mTextView = new TextView(getApplicationContext());
-	            mTextView.setText(vsarray[position]);
-	            mTextView.setTextSize(24);
-	            mTextView.setTextColor(Color.GRAY);
-			}            
-            return mTextView;
+				mTextView.setText(vsarray[position]);
+				mTextView.setTextSize(24);
+				mTextView.setTextColor(Color.GRAY);
+			}
+			return mTextView;
 		}
-		
+
 	}
-	
+
+	private class myLocationListener implements LocationListener {
+		@Override
+		public void onLocationChanged(Location location) {
+			// TODO Auto-generated method stub
+			updateToNewLocation(location);
+
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+
+		}
+	}
+
 	/**
 	 * 获取定位
 	 */
 	private void getLocation() {
 		// 获取位置管理服务
-		LocationManager locationManager;
+
 		String serviceName = Context.LOCATION_SERVICE;
 		locationManager = (LocationManager) this.getSystemService(serviceName);
 		// 查找到服务信息
@@ -157,116 +192,118 @@ public class ViewspotListActivity extends Activity {
 		updateToNewLocation(location);
 		// 设置监听器，自动更新的最小时间为间隔N秒(1秒为1*1000，这样写主要为了方便)或最小位移变化超过N米
 		locationManager.requestLocationUpdates(provider, 5 * 1000, 25,
-				new LocationListener() {
-
-					@Override
-					public void onLocationChanged(Location location) {
-						// TODO Auto-generated method stub
-						updateToNewLocation(location);
-						Log.i(ViewspotListActivity.ACTIVITY_TAG,
-								"时间：" + location.getTime());
-						Log.i(ViewspotListActivity.ACTIVITY_TAG,
-								"经度：" + location.getLongitude());
-						Log.i(ViewspotListActivity.ACTIVITY_TAG,
-								"纬度：" + location.getLatitude());
-						Log.i(ViewspotListActivity.ACTIVITY_TAG,
-								"海拔：" + location.getAltitude());
-					}
-
-					@Override
-					public void onProviderDisabled(String provider) {
-						// TODO Auto-generated method stub
-
-					}
-
-					@Override
-					public void onProviderEnabled(String provider) {
-						// TODO Auto-generated method stub
-
-					}
-
-					@Override
-					public void onStatusChanged(String provider, int status,
-							Bundle extras) {
-						// TODO Auto-generated method stub
-
-					}
-				});
+				mylocalistener);
 	}
 
 	private void updateToNewLocation(Location location) {
-		
+		if (location != null) {
+			Log.i(ViewspotListActivity.ACTIVITY_TAG, "时间：" + location.getTime());
+			Log.i(ViewspotListActivity.ACTIVITY_TAG,
+					"经度：" + location.getLongitude());
+			Log.i(ViewspotListActivity.ACTIVITY_TAG,
+					"纬度：" + location.getLatitude());
+			Log.i(ViewspotListActivity.ACTIVITY_TAG,
+					"海拔：" + location.getAltitude());
+
+			int i = 0;
+			for (i = 0; i < 8; i++) {
+				Double dist = LocateUtil.getDistance(
+						location.getLongitude(),
+						location.getLatitude(), 
+						this.locas[i].getLongitude(), 
+						this.locas[i].getLatitude());
+				Log.i(ViewspotListActivity.ACTIVITY_TAG,
+						"距离" + i + ":" + dist);
+				if (dist < 10) {
+					PlayMP3(i);
+				}
+			}
+		}
 	}
-	
+
 	public VLocation[] createViewLocas() {
 		locas = new VLocation[8];
-		
+
 		locas[0] = new VLocation();
-		locas[0].setLatitude(121.39208623);
-		locas[0].setLongitude(31.22641032);
-		
+		locas[0].setLongitude(121.39208623);
+		locas[0].setLatitude(31.22641032);
+
 		locas[1] = new VLocation();
-		locas[1].setLatitude(121.39217446);
-		locas[1].setLongitude(31.22628108);
-		
+		locas[1].setLongitude(121.39217446);
+		locas[1].setLatitude(31.22628108);
+
 		locas[2] = new VLocation();
-		locas[2].setLatitude(121.39244637);
-		locas[2].setLongitude(31.22569108);
-		
+		locas[2].setLongitude(121.39244637);
+		locas[2].setLatitude(31.22569108);
+
 		locas[3] = new VLocation();
-		locas[3].setLatitude(121.39377488);
-		locas[3].setLongitude(31.22516123);
-		
+		locas[3].setLongitude(121.39377488);
+		locas[3].setLatitude(31.22516123);
+
 		locas[4] = new VLocation();
-		locas[4].setLatitude(121.39563189);
-		locas[4].setLongitude(31.2251556);
-		
+		locas[4].setLongitude(121.39563189);
+		locas[4].setLatitude(31.2251556);
+
 		locas[5] = new VLocation();
-		locas[5].setLatitude(121.396269);
-		locas[5].setLongitude(31.226171);
-		
+		locas[5].setLongitude(121.396269);
+		locas[5].setLatitude(31.226171);
+
 		locas[6] = new VLocation();
-		locas[6].setLatitude(121.39590147);
-		locas[6].setLongitude(31.22807943);
-		
+		locas[6].setLongitude(121.39590147);
+		locas[6].setLatitude(31.22807943);
+
 		locas[7] = new VLocation();
-		locas[7].setLatitude(121.39486268);
-		locas[7].setLongitude(31.22862123);
-		
+		locas[7].setLongitude(121.39486268);
+		locas[7].setLatitude(31.22862123);
+
 		return locas;
 	}
-	
-	public MediaPlayer[] createLocalMp3(){  
-        /** 
-         * 创建音频文件的方法： 
-         * 1、播放资源目录的文件：MediaPlayer.create(MainActivity.this,R.raw.beatit);//播放res/raw 资源目录下的MP3文件 
-         * 2:播放sdcard卡的文件：mediaPlayer=new MediaPlayer(); 
-         *   mediaPlayer.setDataSource("/sdcard/beatit.mp3");//前提是sdcard卡要先导入音频文件 
-         */  
+
+	public MediaPlayer[] createLocalMp3() {
+		/**
+		 * 创建音频文件的方法：
+		 * 1、播放资源目录的文件：MediaPlayer.create(MainActivity.this,R.raw.beatit
+		 * );//播放res/raw 资源目录下的MP3文件 2:播放sdcard卡的文件：mediaPlayer=new
+		 * MediaPlayer(); mediaPlayer.setDataSource("/sdcard/beatit.mp3");//
+		 * 前提是sdcard卡要先导入音频文件
+		 */
 		mps = new MediaPlayer[8];
-		mps[0]=MediaPlayer.create(this,R.raw.zoo1); 
-        mps[0].stop();
-        mps[1]=MediaPlayer.create(this,R.raw.zoo2); 
-        mps[1].stop();
-        mps[2]=MediaPlayer.create(this,R.raw.zoo3); 
-        mps[2].stop();
-        mps[3]=MediaPlayer.create(this,R.raw.zoo4);  
-        mps[3].stop();
-        mps[4]=MediaPlayer.create(this,R.raw.zoo5);  
-        mps[4].stop();
-        mps[5]=MediaPlayer.create(this,R.raw.zoo6);  
-        mps[5].stop();
-        
-        mps[6]=MediaPlayer.create(this,R.raw.zoo7);  
-        mps[6].stop();
-        mps[7]=MediaPlayer.create(this,R.raw.zoo8);  
-        mps[7].stop();
-         
-        return mps;  
-    }  
-	
+		mps[0] = MediaPlayer.create(this, R.raw.zoo1);
+		mps[0].stop();
+		mps[1] = MediaPlayer.create(this, R.raw.zoo2);
+		mps[1].stop();
+		mps[2] = MediaPlayer.create(this, R.raw.zoo3);
+		mps[2].stop();
+		mps[3] = MediaPlayer.create(this, R.raw.zoo4);
+		mps[3].stop();
+		mps[4] = MediaPlayer.create(this, R.raw.zoo5);
+		mps[4].stop();
+		mps[5] = MediaPlayer.create(this, R.raw.zoo6);
+		mps[5].stop();
+
+		mps[6] = MediaPlayer.create(this, R.raw.zoo7);
+		mps[6].stop();
+		mps[7] = MediaPlayer.create(this, R.raw.zoo8);
+		mps[7].stop();
+
+		return mps;
+	}
+
 	private void DisplayToast(String msg) {
 		Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+	}
+	
+	private void PlayMP3(int index) {
+		try {
+			mps[index].prepare();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		mps[index].start();
 	}
 
 }
